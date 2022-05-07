970aff3ba9a19d575ff27832227703963b75a409
Felipe Bari <luizfelipebari@gmail.com>
2022-05-07 01:16:28 -0300

    db render

--- src/database/db.js
+++ src/database/db.js
@@ -4,24 +4,24 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const connection = new Sequelize (
-  process.env.DB_BASE,
-    process.env.DB_USER,
-    process.env.DB_PASS,
+//   process.env.DB_BASE,
+//     process.env.DB_USER,
+//     process.env.DB_PASS,
+//     {
+//         host: process.env.DB_HOST,
+//         port: 5432,
+//         dialect: "postgres",
+//     }
+    'postgres://scstore_user:puXH4NqbQJf9h4FxIUdQJVkvCT51KSGn@dpg-c9qt3cn5f999i81rp0ug-a/scstore',
     {
-        host: process.env.DB_HOST,
-        port: 5432,
-        dialect: "postgres",
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
     }
-    // 'postgres://scstore_user:puXH4NqbQJf9h4FxIUdQJVkvCT51KSGn@dpg-c9qt3cn5f999i81rp0ug-a/scstore',
-    // {
-    //     dialect: 'postgres',
-    //     dialectOptions: {
-    //         ssl: {
-    //             require: true,
-    //             rejectUnauthorized: false
-    //         }
-    //     }
-    // }
 )   
   
     
